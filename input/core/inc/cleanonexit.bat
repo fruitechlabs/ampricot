51b44a473044a1c74a50803501081af9e978d2bf
Omranic <me@omranic.com>
2012-10-21 18:37:00 +0200

    Fixed close-on-exit wrong slash issue

--- input/core/inc/cleanonexit.bat
+++ input/core/inc/cleanonexit.bat
@@ -1,5 +1,5 @@
 @echo off
 for /f "tokens=2 delims==" %%a in ('find "ampricotcleanonexit = " %~dp0ampricot.conf') do set ampricotcleanonexit=%%a
 if %ampricotcleanonexit% == "on" (
-    del /f /q /s "@AMPRICOTINSTALLDIRROOT@/front/tmp/*.*"
+    del /f /q /s "@AMPRICOTINSTALLDIRROOT@\front\tmp\*.*"
 )
